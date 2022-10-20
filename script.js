ba84a8a227b281ff5264c18822e17d763937455d
Volha-Hancharenka <goncharenkolga4567@gmail.com>
2022-10-20 14:44:52 +0300

    hh

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ function setburgerMenu() {
 }
 
 setburgerMenu()
+
+function headerFixed() {
+  const header = document.querySelector('.header')
+  const firstScreen = document.querySelector('[data-observ]')
+  console.log('Is working...')
+  
+  const headerStickyObserver = new IntersectionObserver(([entry]) => {
+    header.classList.toggle('sticky', !entry.isIntersecting)
+  }, { threshold: [1] })
+  
+  if (firstScreen) {
+    headerStickyObserver.observe(firstScreen)
+  }
+}
+
+headerFixed()
